a1c8ed6143ceee7a1d4d6bfd6a9851c32aa38e49
Manujapabodhana <rpmanuja123@gmail.com>
2025-09-30 07:07:38 +0530

    Update App.jsx

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+}from "react-router-dom";
+
 const App = () => {
   return (
     <div>
-      App
+      <Router>
+        <Routes>
+          <Route path='/' element={<Root />} />
+          <Route path='/login' exact element={<Login />} />
+          <Route path='/signup' exact element={<SignUp />} />
+          <Route path='/dashboard' exact element={<Home />} />
+          <Route path='/income' exact element={<Income />} />
+          <Route path='/expense' exact element={<Expense />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
