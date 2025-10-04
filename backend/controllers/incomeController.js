3d6a1a213e136a4cfd9d241a7cd3939e68cb9e89
Manujapabodhana <rpmanuja123@gmail.com>
2025-10-04 10:52:40 +0530

    ss

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -2,10 +2,48 @@ const User = require('../models/User');
 const Income = require('../models/Income');
 
 //ADD INCOME source
-exports.addIncome = async (req, res) => {}
+exports.addIncome = async (req, res) => {
+
+    const userId = req.user.id;
+
+    try{
+        const { icon, source, amount, date } = req.body;
+
+        //validation check for missing fields
+        if(!source || !amount || !date){
+            return res.status(400).json({ message: 'Please provide source, amount, and date', status: 'fail' });
+        }
+
+        const newIncome = new Income({
+            userId,
+            icon,
+            source,
+            amount,
+            date: new Date(date)
+        });
+
+        await newIncome.save();
+
+        return res.status(201).json({ message: 'Income added successfully', status: 'success', data: newIncome });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal server error', status: 'fail' });
+    }
+
+}
 
 //GET INCOMES source
-exports.getAllIncomes = async (req, res) => {}
+exports.getAllIncomes = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const income = await Income.find({ userId }).sort({ date: -1 });
+        res.json({ income });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error', status: 'fail' });
+    }
+}
 
 //DELETE INCOME source
 exports.deleteIncome = async (req, res) => {}   
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
-const incomeRoutes = require('./routes/incomeRoutes');
+const incomeRoutes = require('./routes/IncomeRoutes');
 
 const app = express();
 
